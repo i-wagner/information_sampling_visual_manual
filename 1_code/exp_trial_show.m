85ae9441a83fcde5bfabeb76ebb5a20c04f332a3
I <ilja.wagner@uni-marburg.de>
2021-12-20 17:00:01 +0100

    Simplified how stimuli are drawn
    -- Instead of drawing targets/distractors separately, as before, we are now just drawing everything together

--- 1_code/exp_trial_show.m
+++ 1_code/exp_trial_show.m
@@ -17,35 +17,12 @@ function epar = exp_trial_show(epar, tn)
 
 
     %% Present stimuli
-    % Draw the fixation cross
-    exp_target_draw(epar.window, epar.x_center, epar.y_center, ...
-                    epar.fixsize(2), epar.fixsize(1), epar.fixcol, epar.gray);
-
-    % Draw target(s)
-    if epar.expNo == 2 
-
-        Screen('DrawTextures', epar.window, epar.stim.txt_disp_mask(end), [], ...
-               epar.tex_rect(:, end));
-
-    elseif epar.expNo == 3
-
-        Screen('DrawTextures', epar.window, epar.stim.txt_disp_mask(end-1:end), [], ...
-               epar.tex_rect(:, end-1:end));
-
-    end
-
-    % Draw rest
     epar.stim.txt_disp      = epar.stim.txt_disp(~isnan(epar.stim.txt_disp));
     epar.stim.txt_disp_mask = epar.stim.txt_disp_mask(~isnan(epar.stim.txt_disp_mask));
 
     exp_target_draw(epar.window, epar.fixLoc_px(1), epar.fixLoc_px(2), ...
                     epar.fixsize(2), epar.fixsize(1), epar.fixcol, epar.gray);
-
-        Screen('DrawTextures', epar.window, epar.stim.txt_disp_mask(1:epar.trials.dist_num(tn)), [], ...
-               epar.tex_rect(:, 1:epar.trials.dist_num(tn)));
-
-    end
-
+    Screen('DrawTextures', epar.window, epar.stim.txt_disp_mask, [], epar.tex_rect);
     epar.time(2) = Screen('Flip', epar.window, epar.time(1) + epar.stim_frame);
     if epar.EL
 
