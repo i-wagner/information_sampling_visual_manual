ddf4985598ffad6c74f345ddae4bf3936ba4d84d
Ilja Wagner <ilja.wagner@psychol.uni-giessen.de>
2024-02-23 14:34:59 +0000

    Adding analysis setting
    — Whether to use target for dwell time calculation

--- 3_analysis/settings_analysis.m
+++ 3_analysis/settings_analysis.m
@@ -7,3 +7,10 @@ anal.saccadeDetection.MIN_SACC_DUR = 5;
 %% Fixation check
 anal.fixation.tolerance.DVA = 1.50;
 anal.fixation.checkBounds = [-20, 80];
+
+%% Calculation of dwell times
+% Depending on the expeirmental condition, we either do or do not consider
+% fixations on targets, when calculating dwell times. #
+% We have four conditions: single-target visual, double-target visual,
+% single-target manual, double-target manual
+anal.dwellTimes.useTargets = [false, true, false, true];
