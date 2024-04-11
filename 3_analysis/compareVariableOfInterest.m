7293bf68509db1daad3094709bf74cc474bf9958
Ilja Wagner <ilja.wagner@psychol.uni-giessen.de>
2024-04-11 14:40:40 +0200

    Adding function to compare data from old/new anal.

---
+++ 3_analysis/compareVariableOfInterest.m
@@ -0,0 +1,47 @@
+function compareVariableOfInterest(newPipeline, variableOfInterest, suffix)
+
+    % Compares content of a variable, generated using the new analysis 
+    % pipeline, to it's counterpart, generated using the old pipeline
+    %
+    % Input
+    % newPipeline:
+    % varying type; data from new pipeleine, which to compare to the same
+    % data from the old pipeline
+    %
+    % variableOfInterest:
+    % string; dependent variable of interest:
+    % "proportionValid"
+    % "proportionTrialsWithResponse"
+    %
+    % suffix:
+    % string; use results of old pipeline where only trials
+    % ("_withExclusion") or where trials and subjects where excluded 
+    % ("_allExclusions"). Use "" for no exclusions
+    %
+    % Output
+    % --
+
+    %% Get variable of interest from old pipeline
+    conditionLabels = ["oldGs_visual", "oldGs_manual"];
+    oldPipeline = [];
+    for c = 1:2 % Condition
+        pathToData = strcat("/Users/ilja/Dropbox/12_work/", ...
+                            "mr_informationSamplingVisualManual/2_data/", ...
+                            conditionLabels{c}, suffix, ".mat");
+        thisData = load(pathToData);
+        if strcmp(variableOfInterest, "proportionValid")
+            thisVariable = thisData.exper.prop.val_trials;
+        elseif strcmp(variableOfInterest, "proportionTrialsWithResponse")
+            thisVariable = thisData.exper.prop.resp_trials;
+        end
+        oldPipeline = [oldPipeline, thisVariable];
+    end
+    
+    %% Compare pipelines
+    pipelineResultyMatch = isequaln(newPipeline, oldPipeline);
+    if ~pipelineResultyMatch
+        warning("Results from old and new pipeleine do not match!");
+        keyboard
+    end
+
+end
