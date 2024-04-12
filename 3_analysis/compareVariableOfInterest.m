5f70402bb0e680476a7eaf7d0d12b72419b525e1
Ilja Wagner <ilja.wagner@psychol.uni-giessen.de>
2024-04-12 12:51:12 +0200

    Adding variable of interest to compare function

--- 3_analysis/compareVariableOfInterest.m
+++ 3_analysis/compareVariableOfInterest.m
@@ -35,6 +35,8 @@ function compareVariableOfInterest(newPipeline, variableOfInterest, suffix)
             thisVariable = thisData.exper.prop.resp_trials;
         elseif strcmp(variableOfInterest, "timeLostExcldTrials")
             thisVariable = thisData.exper.timeLostExcldTrials;
+        elseif strcmp(variableOfInterest, "aoiFix")
+            thisVariable = thisData.sacc.propGs.aoiFix_mean;
         end
         oldPipeline = [oldPipeline, thisVariable];
     end
