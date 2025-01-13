865643bbf323999aed44adbbb85f399c06015a81
Ilja Wagner <ilja.wagner@psychol.uni-giessen.de>
2025-01-13 19:36:45 +0100

    Updating figure path

--- 5_outreach/manuscript/figures/initFig.m
+++ 5_outreach/manuscript/figures/initFig.m
@@ -1,4 +1,4 @@
-folder.root = '/Users/ilja/Dropbox/12_work/mr_informationSamplingVisualManual/';
+folder.root = '/Users/ilja/Library/CloudStorage/GoogleDrive-ilja.wagner1307@gmail.com/My Drive/mr_informationSamplingVisualManual/';
 folder.data = strcat(folder.root, '2_data/');
 
 load(strcat(folder.data, 'data_newPipeline.mat')); % Data from experiments
