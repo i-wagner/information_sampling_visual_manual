5c04ef27e70461b212e23b44450ea623c700a0cd
i-wagner <68555601+i-wagner@users.noreply.github.com>
2025-05-25 12:41:04 +0100

    Delete 5_outreach directory

--- 5_outreach/manuscript/figures/initFig.m
+++
@@ -1,5 +0,0 @@
-folder.root = '/Users/ilja/Library/CloudStorage/GoogleDrive-ilja.wagner1307@gmail.com/My Drive/mr_informationSamplingVisualManual/';
-folder.data = strcat(folder.root, '2_data/');
-
-load(strcat(folder.data, 'data_newPipeline.mat')); % Data from experiments
-opt_visuals; % Figure visuals
